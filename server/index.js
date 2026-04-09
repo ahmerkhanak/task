11732c1705b10b710c025af8c528a68918867288
Ahmer <your-email@example.com>
2026-04-09 14:05:32 +0500

    cors update

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://your-app-name.vercel.app"],
+  origin: ["http://localhost:5173", "https://task-ahmerkhanaks-projects.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
